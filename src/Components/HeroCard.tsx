47479b1ff2a0d83614b758f250354121b8915e95
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-05-18 21:58:28 +0530

    Hero Section seperated into card and Section

---
+++ src/Components/HeroCard.tsx
@@ -0,0 +1,55 @@
+import { useAppContext } from "../Context/AppContext";
+
+const HeroCard = () => {
+  const { theme, themeColor } = useAppContext();
+  const currTheme = themeColor[theme];
+  return (
+    <div
+      className="card"
+      style={{
+        height: "300px",
+        background: `${currTheme.boxColor}`,
+      }}
+    >
+      <img
+        src="DaysGone.jpg"
+        className="card-img h-100"
+        style={{ objectFit: "cover" }}
+        alt="Preview"
+      />
+      <div
+        className="card-img-overlay d-flex flex-column justify-content-between gap-2 flex-fill p-5 m-3"
+        style={{
+          color: currTheme.color === "#000000" ? "#ffffff" : "#000000",
+        }}
+      >
+        <p className="border rounded-4 px-4 py-2 position-absolute top-0 end-0 mt-3 me-3">
+          New
+        </p>
+        <p className="fs-2 fw-bold m-0 p-0">Days Gone Remastered</p>
+        <p className="fs-4 m-0 p-0">$78</p>
+        <div className="d-flex gap-3">
+          <button
+            className="btn fw-bold"
+            style={{
+              background: `${currTheme.highLight}`,
+            }}
+          >
+            Purchase
+          </button>
+          <button
+            className="btn"
+            style={{
+              background: `${currTheme.boxColor}`,
+              color: currTheme.color === "#000000" ? "#ffffff" : "#000000",
+            }}
+          >
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroCard;
--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -1,41 +1,10 @@
-import { useAppContext } from "../Context/AppContext";
+import HeroCard from "./HeroCard";
 
 const HeroSection = () => {
-  const { theme, themeColor } = useAppContext();
-  const currTheme = themeColor[theme];
-
   return (
-    <div
-      className="card"
-      style={{
-        minWidth: "800px",
-        height: "300px",
-        background: `${currTheme.boxColor}`,
-        scrollSnapAlign: "start",
-      }}
-    >
-      <img
-        src="DaysGone.jpg"
-        className="card-img h-100"
-        style={{ objectFit: "cover" }}
-        alt="Preview"
-      />
-      <div
-        className="card-img-overlay d-flex flex-column justify-content-between gap-2 flex-fill p-5 m-3"
-        style={{
-          color: currTheme.color === "#000000" ? "#ffffff" : "#000000",
-        }}
-      >
-        <p className="border rounded-4 px-4 py-2 position-absolute top-0 end-0 mt-3 me-3">
-          New
-        </p>
-        <p className="fs-2 fw-bold m-0 p-0">Days Gone</p>
-        <p className="fs-4 m-0 p-0">$78</p>
-        <div className="d-flex gap-3">
-          <button className="btn btn-primary">Purchase</button>
-          <button className="btn btn-primary">Add to cart</button>
-        </div>
-      </div>
+    <div className="p-2">
+      <p className="fw-bold fs-2">Best Offer for You</p>
+      <HeroCard />
     </div>
   );
 };
