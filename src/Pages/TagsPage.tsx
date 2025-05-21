31034b776a00d39bea97c251f929280da2c06f06
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-05-21 19:41:05 +0530

    Replicated the skeleton of the Genre page to all other pages in explore section.

--- src/Pages/TagsPage.tsx
+++ src/Pages/TagsPage.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import GenreCard from "../Components/GenreCard";
 
 const TagsPage = () => {
-  return <div>TagsPage</div>;
+  return (
+    <div>
+      <p className="fw-bold fs-2">Available Genres</p>
+      <div className="row row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
+        {[...Array(5)].map((key) => (
+          <div className="col" key={key}>
+            <GenreCard />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TagsPage;
