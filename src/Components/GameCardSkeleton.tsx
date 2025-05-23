53ceb79ed82e5027137366fb117e4f4530c3c7a3
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-05-23 23:04:52 +0530

    created an skeleton loader for the Game Card.

---
+++ src/Components/GameCardSkeleton.tsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import { useAppContext } from "../Context/AppContext";
+
+const Card = styled.div`
+  height: 320px;
+  overflow: hidden;
+  padding: 1px;
+`;
+
+const GameCardSkeleton = () => {
+  const { theme, themeColor } = useAppContext();
+  const curr = themeColor[theme];
+
+  return (
+    <Card className="card border rounded-4" aria-hidden="true">
+      <div
+        className="card-img-top bg-secondary"
+        style={{ borderRadius: "10px 10px 0 0", height: "170px" }}
+      ></div>
+      <div
+        className="card-body placeholder-glow"
+        style={{
+          background: `${curr.boxColor}`,
+        }}
+      >
+        <div className="d-flex flex-column gap-2 placeholder-glow">
+          <p className="fs-2 m-0 p-0 placeholder col-7 bg-secondary rounded-2"></p>
+          <div className="d-flex align-items-center gap-4">
+            <p className="m-0 placeholder col-2 bg-secondary rounded-2 fs-4"></p>
+          </div>
+          <div className="d-flex gap-2">
+            <button
+              className="btn placeholder col-2 disabled bg-secondary"
+              style={{ border: "none" }}
+            ></button>
+            <a
+              href="#"
+              className="btn fw-bold w-100 placeholder"
+              style={{ background: `${curr.highLight}` }}
+            ></a>
+            <button
+              className="btn placeholder col-3 disabled bg-secondary"
+              style={{ border: "none" }}
+            ></button>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default GameCardSkeleton;
--- src/Components/GameList.tsx
+++ src/Components/GameList.tsx
@@ -1,10 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useGameContext } from "../Context/GameContext";
 import GameCard from "./GameCard";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameList = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const {
     state: { game },
     updateSelectedCategory,
@@ -20,14 +22,27 @@ const GameList = () => {
         updateSelectedCategory(res.data.data);
       } catch (err) {
         alert(err);
+      } finally {
+        setTimeout(() => setIsLoading(false), 2000);
       }
     };
 
     fetch();
   }, [type, id, updateSelectedCategory]);
 
+  if (isLoading)
+    return (
+      <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-5">
+        {[...Array(8)].map((key) => (
+          <div className="col" key={key}>
+            <GameCardSkeleton />
+          </div>
+        ))}
+      </div>
+    );
+
   if (game._id === "") return <p>No Category is Selected.</p>;
-  console.log(game.gamesId);
+
   return (
     <div>
       {game.gamesId.length === 0 ? (
