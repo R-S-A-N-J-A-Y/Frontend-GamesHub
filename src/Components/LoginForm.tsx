30b07312478795c14fee6f6bf87bdb8032aaa79c
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-05-25 10:41:34 +0530

    Added the Form with useForm to maintain the State variables in useForm.

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -2,73 +2,90 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../Context/AuthContext";
+import { useForm, type FieldValues } from "react-hook-form";
 
 const LoginForm = () => {
   const Navigate = useNavigate();
   const { Login } = useAuth();
-  const [userData, setUserData] = useState({
-    email: "",
-    password: "",
-  });
 
-  const HandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserData((currData) => ({
-      ...currData,
-      [e.target.name]: e.target.value,
-    }));
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  // const HandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setUserData((currData) => ({
+  //     ...currData,
+  //     [e.target.name]: e.target.value,
+  //   }));
+  // };
 
   const Validate = () => {
     return true;
   };
 
-  const HandleSubmit = async () => {
+  const onSubmit = (data: FieldValues) => {
     if (!Validate()) return;
-    const res = await CallBackend();
-    if (res.success) {
-      Navigate("/");
-      Login(res.data);
-    }
+    console.log(data);
+    // const res = await CallBackend();
+    // if (res.success) {
+    //   Navigate("/");
+    //   Login(res.data);
+    // }
   };
 
-  const CallBackend = async () => {
-    try {
-      const res = await axios.post("http://localhost:3000/auth/login", {
-        countryCode: "IN",
-        ...userData,
-      });
-      return res.data;
-    } catch (err) {
-      alert(err);
-    }
-  };
+  // const CallBackend = async () => {
+  //   try {
+  //     const res = await axios.post("http://localhost:3000/auth/login", {
+  //       countryCode: "IN",
+  //       ...userData,
+  //     });
+  //     const token = res.headers["x-auth-token"];
+  //     console.log(token);
+  //     console.log(res);
+  //     return res.data;
+  //   } catch (err) {
+  //     alert(err);
+  //   }
+  // };
 
   return (
-    <div
+    <form
       className="text-white border p-5 rounded-4"
       style={{ width: "600px", margin: "0 auto", marginTop: "100px" }}
+      onSubmit={handleSubmit(onSubmit)}
     >
       <h3 className="mb-4">Login</h3>
       <input
-        type="text"
+        {...register("email", { required: true })}
+        type="email"
         className="form-control mb-3"
         placeholder="email"
         name="email"
-        value={userData.email}
-        onChange={(e) => HandleChange(e)}
       />
+      {errors.email?.type === "required" && (
+        <p className="text-danger fw-bold">*Email is Required.</p>
+      )}
       <input
+        {...register("password", { required: true, minLength: 6 })}
         type="text"
         className="form-control mb-3"
         placeholder="password"
         name="password"
-        value={userData.password}
-        onChange={(e) => HandleChange(e)}
       />
-      <button className="btn btn-primary" onClick={HandleSubmit}>
+      {errors.password?.type === "required" && (
+        <p className="text-danger fw-bold">*Password is Required.</p>
+      )}
+      {errors.password?.type === "minLength" && (
+        <p className="text-danger fw-bold">
+          *Password must be atleast 6 Length.
+        </p>
+      )}
+      <button type="submit" className="btn btn-primary">
         Login
       </button>
-    </div>
+    </form>
   );
 };
 
