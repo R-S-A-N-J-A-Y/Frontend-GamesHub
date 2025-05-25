c0d121642ffb069f97d0452922837b0dc47e0355
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-05-25 22:53:51 +0530

    Added the dropdowns filters in ExplorePage.

--- src/Pages/ExplorePage.tsx
+++ src/Pages/ExplorePage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import GeneralGameList from "../Components/GeneralGameList";
 import { useAppContext } from "../Context/AppContext";
+import { ImCross } from "react-icons/im";
 
 const ExplorePage = () => {
   const location = useLocation();
@@ -14,15 +15,95 @@ const ExplorePage = () => {
   }, [location]);
   if (url !== "/explore") return <Outlet />;
   return (
-    <div className="d-flex flex-column gap-5">
-      <div className="d-flex flex-column gap-2">
-        <p className="fw-bolder fs-1 m-0">
-          Top Picks of the
-          <span style={{ color: `${CurrTheme.highLight}` }}> Moment.</span>
-        </p>
-        <p className="fs-5 m-0 text-secondary">
-          Top Games by Popularity and Fresh Releases
-        </p>
+    <div className="d-flex flex-column gap-4">
+      <div className="d-flex flex-column gap-4 pb-2">
+        <div className="d-flex flex-column gap-2">
+          <p className="fw-bolder fs-1 m-0">
+            Top Picks of the
+            <span style={{ color: `${CurrTheme.highLight}` }}> Moment.</span>
+          </p>
+          <p className="fs-5 m-0 text-secondary">
+            Top Games by Popularity and Fresh Releases
+          </p>
+        </div>
+
+        <div className="d-flex gap-4">
+          <div className="dropdown">
+            <button
+              className={`btn  dropdown-toggle rounded-5 fw-bold text-${
+                CurrTheme.name === "light" ? "dark" : "light"
+              } border border-${CurrTheme.name === "light" ? "dark" : "light"}`}
+              style={{ padding: "10px 20px" }}
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Order By
+            </button>
+            <ul className="dropdown-menu">
+              <li>
+                <button className="dropdown-item">Name</button>
+              </li>
+              <li>
+                <button className="dropdown-item">Popularity</button>
+              </li>
+              <li>
+                <button className="dropdown-item">Ratings</button>
+              </li>
+              <li>
+                <button className="dropdown-item">Release Date</button>
+              </li>
+              <li>
+                <button className="dropdown-item d-flex align-items-center text-danger fw-bold gap-2">
+                  <ImCross color="red" />
+                  Clear
+                </button>
+              </li>
+            </ul>
+          </div>
+
+          <div className="dropdown">
+            <button
+              className={`btn  dropdown-toggle rounded-5 fw-bold text-${
+                CurrTheme.name === "light" ? "dark" : "light"
+              } border border-${CurrTheme.name === "light" ? "dark" : "light"}`}
+              style={{ padding: "10px 20px" }}
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Platforms
+            </button>
+
+            <ul className="dropdown-menu">
+              <li>
+                <button className="dropdown-item">PC</button>
+              </li>
+              <li>
+                <button className="dropdown-item">Playstation</button>
+              </li>
+              <li>
+                <button className="dropdown-item">Xbox</button>
+              </li>
+              <li>
+                <button className="dropdown-item">Nintendo</button>
+              </li>
+              <li>
+                <button className="dropdown-item">Android</button>
+              </li>
+
+              <li>
+                <button className="dropdown-item">Apple</button>
+              </li>
+              <li>
+                <button className="dropdown-item d-flex align-items-center text-danger fw-bold gap-2">
+                  <ImCross color="red" />
+                  Clear
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
 
       <GeneralGameList />
