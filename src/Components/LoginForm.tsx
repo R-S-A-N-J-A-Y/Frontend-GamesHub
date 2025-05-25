195a3ae3f0948e3af888a271e968213666270d22
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-05-25 11:27:14 +0530

    Added showPassword and hide password functionality.

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -1,54 +1,54 @@
 import axios from "axios";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../Context/AuthContext";
 import { useForm, type FieldValues } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { GoEye, GoEyeClosed } from "react-icons/go";
+
+const Schema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(6, { message: "*The Password must be atleast 6 Characters Long." }),
+});
+
+type FormData = z.infer<typeof Schema>;
 
 const LoginForm = () => {
   const Navigate = useNavigate();
   const { Login } = useAuth();
+  const [showPassword, setShowPassword] = useState(false);
 
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm();
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(Schema) });
 
-  // const HandleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setUserData((currData) => ({
-  //     ...currData,
-  //     [e.target.name]: e.target.value,
-  //   }));
-  // };
-
-  const Validate = () => {
-    return true;
+  const onSubmit = async (data: FieldValues) => {
+    const res = await CallBackend(data);
+    if (res.success) {
+      Navigate("/");
+      Login(res.data);
+    }
   };
 
-  const onSubmit = (data: FieldValues) => {
-    if (!Validate()) return;
-    console.log(data);
-    // const res = await CallBackend();
-    // if (res.success) {
-    //   Navigate("/");
-    //   Login(res.data);
-    // }
+  const CallBackend = async (userData: FieldValues) => {
+    try {
+      const res = await axios.post("http://localhost:3000/auth/login", {
+        countryCode: "IN",
+        ...userData,
+      });
+      const token = res.headers["x-auth-token"];
+      console.log(token);
+      console.log(res);
+      return res.data;
+    } catch (err) {
+      alert(err);
+    }
   };
-
-  // const CallBackend = async () => {
-  //   try {
-  //     const res = await axios.post("http://localhost:3000/auth/login", {
-  //       countryCode: "IN",
-  //       ...userData,
-  //     });
-  //     const token = res.headers["x-auth-token"];
-  //     console.log(token);
-  //     console.log(res);
-  //     return res.data;
-  //   } catch (err) {
-  //     alert(err);
-  //   }
-  // };
 
   return (
     <form
@@ -58,31 +58,42 @@ const LoginForm = () => {
     >
       <h3 className="mb-4">Login</h3>
       <input
-        {...register("email", { required: true })}
+        {...register("email")}
         type="email"
         className="form-control mb-3"
         placeholder="email"
         name="email"
       />
-      {errors.email?.type === "required" && (
-        <p className="text-danger fw-bold">*Email is Required.</p>
+      {errors.email && (
+        <p className="text-danger fw-bold">{errors.email.message}</p>
       )}
-      <input
-        {...register("password", { required: true, minLength: 6 })}
-        type="text"
-        className="form-control mb-3"
-        placeholder="password"
-        name="password"
-      />
-      {errors.password?.type === "required" && (
-        <p className="text-danger fw-bold">*Password is Required.</p>
-      )}
-      {errors.password?.type === "minLength" && (
-        <p className="text-danger fw-bold">
-          *Password must be atleast 6 Length.
-        </p>
-      )}
-      <button type="submit" className="btn btn-primary">
+
+      <div className="input-group mb-3">
+        <input
+          {...register("password", { required: true, minLength: 6 })}
+          type={`${showPassword ? "text" : "password"}`}
+          className="form-control"
+          placeholder="password"
+          name="password"
+        />
+        {errors.password && (
+          <p className="text-danger fw-bold">{errors.password.message}</p>
+        )}
+
+        <span
+          className="input-group-text"
+          id="basic-addon2"
+          onClick={() => setShowPassword((val) => !val)}
+          style={{ cursor: "pointer" }}
+        >
+          {!showPassword ? <GoEye /> : <GoEyeClosed />}
+        </span>
+      </div>
+
+      <button
+        type="submit"
+        className={`btn btn-primary ${isValid ? "" : "disabled"}`}
+      >
         Login
       </button>
     </form>
