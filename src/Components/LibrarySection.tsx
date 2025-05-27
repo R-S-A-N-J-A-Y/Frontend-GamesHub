257c110a6396eb6dc6a8328874c9ced199c3e0e3
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-05-27 21:34:36 +0530

    Added the Empty Library text in Library Section.

--- src/Components/LibrarySection.tsx
+++ src/Components/LibrarySection.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useAuth } from "../Context/AuthContext";
 import { useGameContext } from "../Context/GameContext";
+import { useNavigate } from "react-router-dom";
 
 export const ArrowIcon = styled(MdDoubleArrow)`
   transition: transform 0.2s ease;
@@ -34,10 +35,12 @@ export interface WatchListDataType {
 }
 
 const LibrarySection = () => {
+  const Navigate = useNavigate();
   const {
     state: { token },
   } = useAuth();
   const { ToggleWatchList } = useGameContext();
+
   const { theme, themeColor } = useAppContext();
   const currTheme = themeColor[theme];
 
@@ -76,13 +79,34 @@ const LibrarySection = () => {
         <ArrowIcon size={25} color={`${currTheme.highLight}`} />
       </div>
 
-      <div className="d-flex flex-column gap-4">
-        {watchList.map((data, index) => (
-          <div key={index}>
+      {watchList.length === 0 ? (
+        <div className="d-flex flex-column align-items-center">
+          <h2 className="fs-3 font-bolder mb-2">
+            Your Library Is{" "}
+            <span style={{ color: `${currTheme.highLight}` }}>Empty.</span>
+          </h2>
+          <p className="text-sm text-muted-foreground mb-4 text-center">
+            You haven’t added any games yet.
+            <br />
+            Found something cool? Add it to your Library!
+          </p>
+          <div>
+            <button
+              className="btn fw-bold w-100"
+              style={{ background: `${currTheme.highLight}` }}
+              onClick={() => Navigate("/explore")}
+            >
+              Browse Games
+            </button>
+          </div>
+        </div>
+      ) : (
+        watchList.map((data, index) => (
+          <div key={index} className="d-flex flex-column gap-3">
             <LibraryCard game={data} onClick={handleRemove} />
           </div>
-        ))}
-      </div>
+        ))
+      )}
     </Wrapper>
   );
 };
