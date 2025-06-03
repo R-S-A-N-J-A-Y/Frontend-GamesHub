8950cb3d97abb34ad39c84c491e7b970dce9d746
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-06-03 20:34:14 +0530

    Removed the pagination from the Explore Category page.

--- src/Pages/ExploreCategoryPage.tsx
+++ src/Pages/ExploreCategoryPage.tsx
@@ -10,34 +10,20 @@ const ExploreCategoryPage = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const { type } = useParams<{ type: string }>();
-  const { state, updateCategory, updateCategoryType } = useGameContext();
-  const [currPage, setCurrPage] = useState(0);
-  const [startPage, setStartPage] = useState(0);
-
-  //To reset the pages to initial for every different category.
-  useEffect(() => {
-    setCurrPage(0);
-    setStartPage(0); //To reset the pages to initial for every different category.
-    updateCategoryType(type || ""); //Sets the Type first To avoid the Rendering of the leftOver page by previous Type.
-  }, [updateCategoryType, type]);
+  const { state, updateCategory } = useGameContext();
 
   useEffect(() => {
-    // To avoid the Fetching data for the left-over page value
-    if (type !== state.category.type) return;
     setIsLoading(true);
     const fetchData = async () => {
       try {
         // Call To Backend .
         let url = type;
         if (type === "platforms") url = "platformsv";
-        const res = await axios.get(`http://localhost:3000/user/${url}/`, {
-          params: { page: currPage, limit: 20 },
-        });
+        const res = await axios.get(`http://localhost:3000/user/${url}/`, {});
 
         // Data Retrival from result.
         const data = res.data.data || [];
 
-        console.log(data);
         // Updating the GameContext
         updateCategory(type || "", data);
       } catch (err) {
@@ -50,7 +36,7 @@ const ExploreCategoryPage = () => {
     };
 
     fetchData();
-  }, [updateCategory, currPage, state.category.type, type]);
+  }, [updateCategory, type]);
 
   if (isLoading)
     return (
@@ -79,81 +65,6 @@ const ExploreCategoryPage = () => {
           ))}
         </div>
       )}
-
-      {/* Pagination  */}
-      <nav aria-label="Page navigation example">
-        <ul className="pagination justify-content-center">
-          <li className={`page-item ${currPage === 0 ? "disabled" : ""}`}>
-            <h1
-              className="page-link"
-              style={{ cursor: "pointer" }}
-              onClick={() => {
-                const newPage = currPage - 1;
-                setCurrPage(newPage);
-                if (currPage !== 1) {
-                  setStartPage(currPage - 2);
-                }
-              }}
-            >
-              Previous
-            </h1>
-          </li>
-          <li
-            className={`page-item ${startPage === currPage ? "disabled" : ""}`}
-          >
-            <h1
-              className="page-link"
-              onClick={() => setCurrPage(startPage)}
-              style={{ cursor: "pointer" }}
-            >
-              {startPage + 1}
-            </h1>
-          </li>
-          <li
-            className={`page-item ${
-              startPage + 1 === currPage ? "disabled" : ""
-            }`}
-          >
-            <h1
-              className="page-link"
-              onClick={() => setCurrPage(startPage + 1)}
-              style={{ cursor: "pointer" }}
-            >
-              {startPage + 2}
-            </h1>
-          </li>
-          <li
-            className={`page-item ${
-              startPage + 2 === currPage ? "disabled" : ""
-            }`}
-          >
-            <h1
-              className="page-link"
-              onClick={() => setCurrPage(startPage + 2)}
-              style={{ cursor: "pointer" }}
-            >
-              {startPage + 3}
-            </h1>
-          </li>
-          <li
-            className={`page-item ${
-              state.category.data.length === 0 ? "disabled" : ""
-            }`}
-          >
-            <h1
-              className="page-link"
-              style={{ cursor: "pointer" }}
-              onClick={() => {
-                const newPage = currPage + 1;
-                setStartPage(newPage - 1);
-                setCurrPage(newPage);
-              }}
-            >
-              Next
-            </h1>
-          </li>
-        </ul>
-      </nav>
     </div>
   );
 };
