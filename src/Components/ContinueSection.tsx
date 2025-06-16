b8bcfd9c6e965ecc71ee110f3ca27c6c18c1a881
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-06-16 21:15:30 +0530

    Updated Continue Section to fetch the Users Recently Viewed Game.

--- src/Components/ContinueSection.tsx
+++ src/Components/ContinueSection.tsx
@@ -1,10 +1,18 @@
 import { useRef, useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import GameCard from "./GameCard";
+import { useAuth } from "../Context/AuthContext";
+import axios from "axios";
+import type { Gamedata } from "../Context/GameContext";
 
 const ContinueSection = () => {
   const carouselRef = useRef<HTMLDivElement | null>(null);
   const [width, setWidth] = useState(0);
+
+  const {
+    state: { token },
+  } = useAuth();
+  const [gameData, setGameData] = useState<Gamedata[]>([]);
 
   useEffect(() => {
     const carousel = carouselRef.current;
@@ -15,21 +23,20 @@ const ContinueSection = () => {
     }
   }, []);
 
-  const SampleGameData = {
-    _id: "string",
-    name: "string",
-    coverImageUrl:
-      "https://ik.imagekit.io/sanjayvault/GamesHub/Game%20Previews/Days%20Gone/Preview/DaysGone.png",
-    peopleAdded: 0,
-    ratings: 0,
-    likes: 0,
-    price: 0,
-
-    platforms: [{ _id: "123", parentPlatform: { name: "PC", _id: "akhd" } }],
-
-    liked: true, //Only Available when User Logged In
-    watched: true,
-  };
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const result = await axios.get(
+          "http://localhost:3000/user/recentlyWatched/",
+          { headers: { "x-auth-token": token } }
+        );
+        setGameData(result.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetch();
+  }, [token]);
 
   return (
     <div>
@@ -52,9 +59,9 @@ const ContinueSection = () => {
           className="d-flex gap-5"
           whileTap={{ cursor: "grabbing" }}
         >
-          {[...Array(5)].map((_, i) => (
-            <motion.div key={i} style={{ flex: "0 0 auto" }}>
-              <GameCard game={SampleGameData} cardWidth="300px" />
+          {gameData.map((game, _) => (
+            <motion.div key={_} style={{ flex: "0 0 auto" }}>
+              <GameCard game={game} cardWidth="300px" />
             </motion.div>
           ))}
           <div style={{ flex: "0 0 auto", width: "10px" }} />
