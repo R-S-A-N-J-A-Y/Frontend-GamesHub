80025e0362e5e4edfbb4006c9a7f935f99b0a0c5
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-06-25 21:45:07 +0530

    bug fix in the Platforms of Game Card.

--- src/Components/Platforms.tsx
+++ src/Components/Platforms.tsx
@@ -13,9 +13,13 @@ interface Props {
 }
 
 const Platforms = ({ platforms, size }: Props) => {
+  const uniquePlatforms = Array.from(
+    new Map(platforms.map((p) => [p.parentPlatform._id, p])).values()
+  );
+
   return (
     <>
-      {platforms.map((obj) => (
+      {uniquePlatforms.map((obj) => (
         <>
           {obj.parentPlatform.name === "PC" && (
             <FaWindows size={size} color="white" />
