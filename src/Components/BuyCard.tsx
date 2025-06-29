02983fcf994a0ea5657950769e4b55c153834ca8
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-06-29 11:23:39 +0530

    implemented the responsiveness for the buycard in cart page.

--- src/Components/BuyCard.tsx
+++ src/Components/BuyCard.tsx
@@ -2,11 +2,28 @@ import { MdCurrencyRupee } from "react-icons/md";
 import { ArrowIcon } from "./LibrarySection";
 import { useAppContext } from "../Context/AppContext";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 interface Props {
   games: number;
   total: number;
 }
+
+const Wrapper = styled.section<{ $background: string }>`
+  background: ${({ $background }) => $background};
+
+  @media (max-width: 930px) {
+    flex-direction: column;
+  }
+`;
+
+const PriceSegment = styled.section`
+  width: 300px;
+
+  @media (max-width: 390px) {
+    width: 200px;
+  }
+`;
 
 const BuyCard = ({ games, total }: Props) => {
   const { theme, themeColor } = useAppContext();
@@ -14,11 +31,11 @@ const BuyCard = ({ games, total }: Props) => {
   const Navigate = useNavigate();
 
   return (
-    <div
+    <Wrapper
+      $background={curr.boxColor}
       className="p-5 d-flex justify-content-between align-items-center gap-3 border rounded-4"
-      style={{ background: curr.boxColor }}
     >
-      <div>
+      <section>
         <button
           className={`border border-2 rounded-3 fs-5 fw-bold px-4 py-2 d-flex gap-3 align-items-center text-${
             curr.name === "dark" ? "white" : "black"
@@ -31,8 +48,8 @@ const BuyCard = ({ games, total }: Props) => {
           <ArrowIcon style={{ transform: "rotate(180deg)" }} size={25} />
           Continue Exploring
         </button>
-      </div>
-      <div className=" fs-5 fw-bold" style={{ width: "300px" }}>
+      </section>
+      <PriceSegment className=" fs-5 fw-bold">
         <div className="row mb-3">
           <div className="col-6">
             <p className="m-0 p-0">No. of Games:</p>
@@ -69,15 +86,15 @@ const BuyCard = ({ games, total }: Props) => {
         <div className="row">
           <div className="col-12">
             <button
-              className="p-2 btn w-100 fw-bolder fs-5 text-white"
+              className="p-2 btn w-100 fw-bolder fs-5 text-white cursor-pointer"
               style={{ background: curr.highLight }}
             >
               Checkout
             </button>
           </div>
         </div>
-      </div>
-    </div>
+      </PriceSegment>
+    </Wrapper>
   );
 };
 
