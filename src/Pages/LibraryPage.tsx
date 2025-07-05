109e94a028778517d639c8a756c3593ca8698f7f
R-S-A-N-J-A-Y <sanjay638291@gmail.com>
2025-07-05 13:45:36 +0530

    ui fetching is completed in library section

--- src/Pages/LibraryPage.tsx
+++ src/Pages/LibraryPage.tsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Platform } from "../Context/GameContext";
 import { useAppContext } from "../Context/AppContext";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useAuth } from "../Context/AuthContext";
+import GameCard from "../Components/GameCard";
 
 type GameData = {
   _id: string;
@@ -17,13 +20,46 @@ type GameData = {
 const LibraryPage = () => {
   const { theme, themeColor } = useAppContext();
   const currTheme = themeColor[theme];
+  const {
+    state: { token },
+  } = useAuth();
+
   const Navigate = useNavigate();
 
-  const [games, setGames] = useState<GameData | null>(null);
+  const [games, setGames] = useState<GameData[] | null>(null);
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const result = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/user/watchListPreview`,
+          {
+            headers: { "x-auth-token": token },
+          }
+        );
+        setGames(result.data.data);
+      } catch (err) {
+        console.log(err);
+        alert(err);
+      }
+    };
+    fetch();
+  }, [token]);
 
   return (
     <div>
-      <p className="fw-bold fs-3 m-0 mb-4">Library</p>
+      <div className="mb-5">
+        <p className="fw-bold fs-3 m-0 mb-2">
+          Game{" "}
+          <span className="fs-3" style={{ color: `${currTheme.highLight}` }}>
+            Archive
+          </span>
+        </p>
+        <h5 className="text-secondary fw-bold">
+          Everything you love, all in one place.
+        </h5>
+      </div>
+
       {!games ? (
         <div className="d-flex flex-column align-items-center">
           <h2 className="fs-3 font-bolder mb-2">
@@ -46,7 +82,13 @@ const LibraryPage = () => {
           </div>
         </div>
       ) : (
-        <div>ge</div>
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 gy-4">
+          {games.map((game, key) => (
+            <div key={key} className="col">
+              <GameCard game={{ ...game, liked: true, watched: true }} />
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
